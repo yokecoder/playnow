6e63c2b7d595dd440228c9af9ca2b9da7a5626fe
yokecoder <hariyoki0719@gmail.com>
2025-02-03 19:28:48 +0530

    minor changes

--- client/src/pages/yt.jsx
+++ client/src/pages/yt.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import SearchBar from "../comps/searchbar"
+import SearchBar from "../comps/searchbar";
+import VideoPlayer from "../comps/videoplayer";
 
 export default function Yt(){
   const [search, setSearch] = useState("")
+  
+  
+  
   return (
   <>
     <div>
       
       <SearchBar value={search} onChange={setSearch} onCancel={() => setSearch("")}  />
       <div className="search-results">
-        
+        <VideoPlayer url="https://www.youtube.com/embed/playlist?list=PLwheXbz_XBtltsxCn00Hkdc2Dqa9t6wNd&si=dzP4AGQb-8DMpBVs"/>
       </div>
     </div>
   </>
