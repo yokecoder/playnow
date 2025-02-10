af0797d439be5ec74aee3057988c03eb2b08b948
yokecoder <hariyoki0719@gmail.com>
2025-02-10 18:12:11 +0530

    updated cookies 1

--- server/routes/ytapis.js
+++ server/routes/ytapis.js
@@ -5,11 +5,7 @@ const fs = require("fs");
 
 const router = express.Router();
 
-const randomUserAgents = [
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-];
+
 
 const youtubeHeadersMiddleware = (req, res, next) => {
   req.ytdlOptions = {
